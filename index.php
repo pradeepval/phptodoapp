911af5b6d2c1c680a820641c6186b214c8c16e9b
Pradeep Val <pradeephann09@gmail.com>
2022-04-27 15:11:53 +0530

    git ignore

--- index.php
+++ index.php
@@ -3,35 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width,
+    <meta name="viewport" content="width=device-width">
     <title>Document</title>
     <link rel="stylesheet" href="style.css">
 </head>
-<body>
-    <h1 class="top-heading">CCCR TODO LIST APLLICATION</h1>
+<body>    
     <div class="container">
-    <form action="" method="post">
-     <div class="input-container">
-         <input type="text" name="inputBox" id="inputBox">
-         <button type="submit" id="add">ADD</button>
-     </div>
-      <ul class="list">
-          <li class="item">
-             <p>item 1</p>
-             <button type="submit" name="" id="check"><i class="fa-solid fa-check"></i></button>
-             <button type="submit" name="" id="delete"><i class="fa-solid fa-delete-left"></i></button>
-          </li>
-      </ul>
-      <hr>
-      <ul class="list">
-          <li class="item">
-             <p>item 1</p>
-             <button type="submit" name="" id="check"><i class="fa-solid fa-check"></i></button>
-             <button type="submit" name="" id="delete"><i class="fa-solid fa-delete-left"></i></button>
-          </li>
-      </ul>
-    </form>
-</div>
+        <h1 class="top-heading"> TODO LIST APLLICATION</h1>
+        <form action="" method="post">
+        <div class="input-container">
+            <input type="text" name="inputBox" id="inputBox">
+            <button type="submit" id="add">ADD</button>
+        </div>
+        <ul class="list">
+            <li class="item">
+                <p>item 1</p>
+                <button type="submit" name="" id="check"><i class="fa-solid fa-check"></i></button>
+                <button type="submit" name="" id="delete"><i class="fa-solid fa-delete-left"></i></button>
+            </li>
+        </ul>
+        <hr>
+        <ul class="list">
+            <li class="item">
+                <p>item 1</p>
+                <button type="submit" name="" id="check"><i class="fa-solid fa-check"></i></button>
+                <button type="submit" name="" id="delete"><i class="fa-solid fa-delete-left"></i></button>
+            </li>
+        </ul>
+        </form>
+    </div>
 <script src="https://kit.fontawesome.com/48d7d484a4.js" crossorigin="anonymous"></script>
 </body>
 </html>
