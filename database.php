7cbbdd6e735628db43042a678a80c6cefc40b8b0
Pradeep Val <pradeephann09@gmail.com>
2022-05-03 12:39:15 +0530

    database file fixed

--- database.php
+++ database.php
@@ -1,27 +1,27 @@
 <?php
 function makeConnection()
 {
-  $host     = "localhost";
-  $username = "root";
-  $password = "";
-  $dbname   = "todoapp1";
-  $con      = new mysqli($host, $username, $password, $dbname);
-  if ($con->connect_error) {
-    echo "There is an error in Database connection" . $con->connect_error;
-  }
-  return $con;
-  /*  echo "successfully connected";*/
+ $host     = "localhost";
+ $username = "root";
+ $password = "";
+ $dbname   = "todoapp1";
+ $con      = new mysqli($host, $username, $password, $dbname);
+ if ($con->connect_error) {
+  echo "There is an error in Database connection" . $con->connect_error;
+ }
+ return $con;
+//  echo "successfully connected";
 }
 function addItems($value)
 {
-  $con   = makeConnection();
-  $query = "INSERT INTO todolist(id,item,status) VALUES(NULL,'$value', '0')";
-  $con->query($query);
+ $con   = makeConnection();
+ $query = "INSERT INTO todolist(id,item,status) VALUES(NULL,'$value', '0')";
+ $con->query($query);
 }
 function getItems()
 {
-  $con    = makeConnection();
-  $query  = "SELECT id,item from todolist WHERE status='0'";
-  $result = $con->query($query);
-  return $result;
+ $con    = makeConnection();
+ $query  = "SELECT id,item from todolist WHERE status='0'";
+ $result = $con->query($query);
+ return $result;
 }
